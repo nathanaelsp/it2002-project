ae093b7cfdc85781ae22414491bbd2963b9796ca
Gede Arya Chandradhana <83939939+gac01@users.noreply.github.com>
2022-02-11 00:07:58 +0800

    Answers for Qns 2a and 2d

--- answers2.sql
+++ answers2.sql
@@ -9,15 +9,22 @@
 % Student numbers of ALL team members                                         %
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% member 1:
-% member 2:
-% member 3:
+% member 1: A0219743Y
+% member 2: A0200704Y
+% member 3: A0184578L
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                             %
 % Question 2.a                                                                %
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SELECT cc_type
+FROM customer
+GROUP BY cc_type
+HAVING COUNT(cc_type) >= (SELECT MAX(total_users) 
+						  FROM (SELECT COUNT(cc_type) AS total_users
+								FROM customer
+								GROUP BY cc_type) AS max_value)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                             %
@@ -36,3 +43,11 @@
 % Question 2.d                                                                %
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SELECT c.full_name
+FROM customer AS c, portfolio AS p1, stock AS s1
+WHERE c.email = p1.c_email AND p1.s_symbol = s1.stock_symbol
+GROUP BY full_name
+HAVING SUM(s1.price) >= (SELECT MAX(total_value)
+						 FROM(SELECT SUM(s2.price) AS total_value
+						FROM portfolio AS p2 INNER JOIN stock AS s2 ON p2.s_symbol = s2.stock_symbol
+						GROUP BY c_email) AS port_values)
