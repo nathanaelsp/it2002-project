3cd83f4529230ec25e1b490a778a94eaf3c67856
Nathanael Septianto Pratama <nathanael.septianto@u.nus.edu>
2022-02-11 14:22:51 +0800

    Update answers1.sql

--- answers1.sql
+++ answers1.sql
@@ -93,8 +93,8 @@ INSERT INTO stock (stock_name, stock_symbol, industry, sector, market, price) VA
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	
 
 INSERT INTO portfolio (c_email, s_symbol,quantity)
-SELECT c.email,s.stock_symbol,ROUND(RANDOM()*99) + 1
-FROM customer AS c, stock AS s
+SELECT c.email,s.stock_symbol,num_series
+FROM customer AS c, stock AS s, generate_series(1,100) AS num_series
 ORDER BY RANDOM()
 LIMIT 500;
 
