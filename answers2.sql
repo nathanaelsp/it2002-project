ab9444ce16beb5f3966e8c25f6af31aece03b91b
Nathanael Septianto Pratama <nathanael.septianto@u.nus.edu>
2022-02-15 12:46:45 +0800

    Update answers2.sql

--- answers2.sql
+++ answers2.sql
@@ -18,6 +18,8 @@
 % Question 2.a                                                                %
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Return Credit Card type with the highest numbers of users
+
 SELECT cc_type
 FROM customer
 GROUP BY cc_type
@@ -31,7 +33,7 @@ HAVING COUNT(cc_type) >= (SELECT MAX(total_users)
 % Question 2.b                                                                %
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Filter out email addresses whose owner hold any IBM stocks (from customer table)
+% Filter out email addresses whose owner hold any IBM stocks (from customer table) and return the remaining full name(s) and email(s)
 
 SELECT c.full_name, c.email
 FROM customer c
@@ -49,15 +51,27 @@ ORDER BY c.email;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Take the sum product of the quantity of IBM stocks managed by the portal (from portfolio table) and the stock price (from stock table)
 
-SELECT sum(p.quantity * s.price) AS ibm_total_value
+SELECT sum(p.quantity * s.price) AS total_value
 FROM portfolio p, stock s
 WHERE s.stock_symbol = p.s_symbol AND s.stock_name = 'International Business Machines Corporation';
+
+SELECT s1.total_quantity * s2.price AS total_value
+FROM (SELECT SUM(quantity) AS total_quantity
+      FROM portfolio
+      WHERE s_symbol = 'IBM'
+      GROUP BY s_symbol ) AS s1,
+      (SELECT price
+       FROM stock
+       WHERE stock_symbol = 'IBM' 
+      LIMIT 1) AS s2 ;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                                                                             %
 % Question 2.d                                                                %
 %                                                                             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Return the full name of customer(s) with the largest portfolio total value
+
 SELECT c.full_name
 FROM customer AS c, portfolio AS p1, stock AS s1
 WHERE c.email = p1.c_email AND p1.s_symbol = s1.stock_symbol
